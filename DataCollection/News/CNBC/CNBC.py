6285a446418b4a8a9090fd2c1e7a29958ee07062
Slayer2084 <90677284+Slayer2084@users.noreply.github.com>
2022-02-15 19:29:12 +0100

    completed CNBC

--- DataCollection/News/CNBC/CNBC.py
+++ DataCollection/News/CNBC/CNBC.py
@@ -13,29 +13,38 @@ class CNBCSpider(scrapy.Spider):
     custom_settings = {
         'LOG_LEVEL': 'WARN',
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1',
+        'ROBOTSTXT_OBEY': False,
+        # 'PROXY_POOL_ENABLED': True,
+        # 'DOWNLOADER_MIDDLEWARES': {
+        #     'scrapy_proxy_pool.middlewares.ProxyPoolMiddleware': 610,
+        #     'scrapy_proxy_pool.middlewares.BanDetectionMiddleware': 620,
+        # }
     }
 
     def start_requests(self):
         queries = ['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         for query in queries:
-            yield scrapy.Request(url.format(query, 0))
+            yield scrapy.Request(url.format(query, 0), callback=self.parse)
 
     def parse(self, response):
         metadata = response.json()["metadata"]
         total_pages = metadata["totalpage"]
         current_page = metadata["pagerequested"]
 
+        # print(re.search(r'query=(.*?)%22', response.url).group(1) + ": " + str(current_page))
+
         if total_pages and current_page:
             if int(current_page) == 1:
-                for i in range(0, int(total_pages)*100, 100):
-                    print(response.url.replace("endindex=(.*?)&batchsize", str(i)))
-                    yield scrapy.Request(url=response.urljoin(str(i)))
+                for i in range(100, (int(total_pages)+1)*100, 100):
+                    yield scrapy.Request(url=url.format(re.search(r'query=(.*?)%22', response.url).group(1), i), callback=self.parse)
 
         for result in response.json()["results"]:
-            if result["cn:type"] != "cnbcvideo":
-                yield scrapy.Request(result["cn:liveURL"], callback=self.parse_articles)
+            if result["brand"] == "cnbc":
+                if result["cn:type"] != "cnbcvideo":
+                    yield scrapy.Request(result["cn:liveURL"], callback=self.parse_articles)
 
     def parse_articles(self, response):
+        # print(".", flush=False, end="")
         yield {
             'Title': response.css(".ArticleHeader-headline ::text").get(),
             'Category': response.css(".ArticleHeader-eyebrow ::text").get(),
